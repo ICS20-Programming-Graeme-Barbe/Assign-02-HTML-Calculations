b378472f7f3a234c31b8d712e6abbff1373a1c36
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-04-05 01:46:29 +0000

    index.html

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@
 
 // This function calculates the surface area and volume of a cone
 'use strict'
-function answer() {
+function calculate() {
 	// Declares variables 
 	let radius = parseFloat(document.getElementById('radius-of-base').value)
 	let height = parseFloat(document.getElementById('height-of-cone').value)
 	let slantHeight = parseFloat(document.getElementById('slant-height').value)
-	let unit = document.getElementById('unit-of-measurement').innerHTML
+	let units = document.getElementById('unit-of-measurement').innerHTML
 	//Calulates SA and volume
 	let surfaceArea = Math.PI * (radius **2) + Math.PI * radius * slantHeight
 	let volume = 1 / 3 * (Math.PI * (radius **2)) * height
 	//Answers
-	document.getElementById('surface-area').innerHTML = 'The surface area is ' + surfaceArea + unit
-	document.getElementById('volume').innerHTML = 'The volume is ' + surfaceArea + unit + "²"
+	document.getElementById('surface-area').innerHTML = 'The surface area is ' + surfaceArea.toFixed(2) + unit
+	document.getElementById('volume').innerHTML = 'The volume is ' + volume.toFixed(2) + units + "²"
 }
